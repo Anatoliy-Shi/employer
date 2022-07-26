1d2b66dda90c20da2304074236f9367e21c96437
Анатолий Шипилов <asmatol@yandex.ru>
2022-07-26 17:21:36 +0300

    add deploy gh-pages

---
+++ src/components/app/app.js
@@ -0,0 +1,122 @@
+import {Component} from "react";
+
+import AppInfo from '../app-info/app-info';
+import SearchPanel from '../search-panel/search-panel';
+import AppFilter from '../app-filter/app-filter';
+import EmployeesList from '../employees-list/employees-list';
+import EmployeesAddForm from '../employees-add-form/employees-add-form';
+import './app.css';
+
+
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data : [
+                {name: 'Alex. D.', salary: 4972, like: true, increase: true, id: 1},
+                {name: 'Nina. N.', salary: 5100, like: true, increase: true, id: 2},
+                {name: 'Michael. E.', salary: 890, like: false, increase: false, id: 3},
+            ],
+            term: '',
+            filter: 'all'
+        }
+        this.maxId = 4;
+    }
+
+    deleteItem = (id) => {
+        this.setState(({data}) => {
+            return {
+                data: data.filter(item => item.id !== id)
+            }
+        })
+    }
+
+    addItem = (name, salary) => {
+        const newItem = {
+            name,
+            salary,
+            increase: false,
+            like: false,
+            id: this.maxId++
+        }
+        this.setState(({data}) => {
+            let newArr;
+            if(newItem.name.length < 3 || newItem.salary < 1) {
+                return false
+            } else {
+                newArr = [...data, newItem];
+            }
+            return {
+                data: newArr
+            }
+        });
+    }
+
+    onToggleProp = (id, prop) => {
+        this.setState(({data}) => ({
+            data: data.map(item => {
+                if (item.id === id) {
+                    return {...item, [prop]: !item[prop]}
+                }
+                return item;
+            })
+        }))
+    }
+
+    searchEmp = (items, term) => {
+        if (term.length === 0) {
+            return items
+        }
+        return items.filter(el => {
+            return el.name.indexOf(term) > -1
+        })
+    }
+
+    onUpdateSearch = (term) => {
+        this.setState({term})
+    }
+
+    filterPost = (items, filter) => {
+        switch (filter) {
+            case 'like':
+                return items.filter(item => item.like)
+            case 'moreTren1000':
+                return  items.filter(item => item.salary > 1000)
+            default:
+                return items
+        }
+    }
+
+    onFilterSelect = (filter) => {
+        this.setState({filter})
+    }
+
+    render() {
+        const {data, term, filter} = this.state
+        const employees = this.state.data.length;
+        const increased = this.state.data.filter(item => item.increase).length
+        const visibleData = this.filterPost(this.searchEmp(data, term), filter)
+
+        return (
+            <div className="app">
+                <AppInfo increased = {increased} employees = {employees} />
+
+                <div className="search-panel">
+                    <SearchPanel onUpdateSearch = {this.onUpdateSearch}/>
+                    <AppFilter filter = {filter} onFilterSelect={this.onFilterSelect}/>
+                </div>
+
+                <EmployeesList
+                    data = {visibleData}
+                    onDelete = {this.deleteItem}
+                    onToggleProp = {this.onToggleProp}/>
+                <EmployeesAddForm onAdd={this.addItem}/>
+            </div>
+        );
+    }
+
+
+}
+
+export default App;
